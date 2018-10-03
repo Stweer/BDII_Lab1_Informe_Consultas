673b44d04a19c99b97307d13890a5f419ee06907
Stweer <38274756+Stweer@users.noreply.github.com>
2018-10-03 13:56:13 -0500

    Consultas Actividad 4.8.1

---
+++ Laboratorio1_Consultas/Database1/ScriptsConsultas/Actividad8/4.8.1.sql
@@ -0,0 +1,15 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+SELECT l.location_id,l.street_address,l.city,l.state_province,c.country_name
+from locations as l
+join countries as c
+on l.country_id = c.country_id
